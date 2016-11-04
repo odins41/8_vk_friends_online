9450927e33657e5a3269c31b80a14eb0a7783168
odins41 <odins41@mail.ru>
2016-11-04 23:51:22 +0300

    Update vk_friends_online.py

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,29 +1,29 @@
 import vk
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5708591
 
 
 def get_user_login():
-    pass
-
+	login = input('Введите логин\n')
+	return login 
 
 def get_user_password():
-    pass
+	password = input('Введите пароль\n')
+	return password
 
 
 def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+	session = vk.AuthSession(app_id=APP_ID, user_login=login, user_password=password, scope='friends')
+	api = vk.API(session)
+	dict_of_id = api.friends.getOnline(order = 'hints')
+	dict_of_name = api.users.get(user_ids=dict_of_id)
+	return dict_of_name 	
 
 
 def output_friends_to_console(friends_online):
-    pass
+	print ('Список ваших друзей онлайн')
+	for user in friends_online:
+		print ('{} {}'.format(user['last_name'],user['first_name']))
 
 if __name__ == '__main__':
     login = get_user_login()
